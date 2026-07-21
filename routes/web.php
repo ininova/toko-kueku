9f21f498fa0029b135a17669573f018282002e18
robbishimozuki <robbishimozuki@gmail.com>
2026-07-21 22:27:31 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -91,16 +91,7 @@ Route::middleware('auth:moonshine')->group(function () {
     )->name('orders.invoice');
 });
 
-Route::get('/logout', function () {
 
-    Auth::logout();
-
-    request()->session()->invalidate();
-
-    request()->session()->regenerateToken();
-
-    return redirect('/');
-})->name('logout');
 
 Route::get(
 
